c4993badaabe3eaf25f3a8626cb9f9b1daeef6c6
FriedStan <66070030@kmitl.ac.th>
2025-08-14 17:08:25 +0700

    pwm-led

---
+++ pwm.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+ledpin = 12
+
+system GPIO.setup(ledpin, GPIO.OUT)
+pi_pwm = GPIO.PWM(ledpin, 1000)
+pi_pwm.start(0)
+
+while True:
+    for duty in range(0, 101, 1):
+        pi_pwm.ChangeDutyCycle(duty)
+        sleep(0.05)
