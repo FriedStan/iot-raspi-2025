cc42a9901af059f507d17f20c53239cc07ac42e4
FriedStan <66070030@kmitl.ac.th>
2025-08-14 17:14:12 +0700

    pwm-led

--- analog_led.py
+++ analog_led.py
@@ -1,7 +1,16 @@
 import time
+import RPi.GPIO as GPIO
 import spidev
 spi = spidev.SpiDev()
 spi.open(0, 0)
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+ledpin = 12
+
+GPIO.setup(ledpin, GPIO.OUT)
+pi_pwm = GPIO.PWM(ledpin, 1000)
+pi_pwm.start(0)
 
 def ReadChannel(channel):
     adc = spi.xfer2([6 | (channel & 4) >> 2, (channel & 3) << 6, 0])
@@ -13,4 +22,5 @@ while True:
         reading = ReadChannel(0)
         voltage = (reading * 3.3) / 4096
         print(f"Reading={reading}\t Voltage={voltage}")
+        pi_pwm.ChangeDutyCycle(reading / 4096 * 100)
         time.sleep(0.2)
