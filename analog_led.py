b0f7a565d33fda6396088cb8f34b58c4d4820d96
FriedStan <66070030@kmitl.ac.th>
2025-08-14 16:00:27 +0700

    analog-led

--- analog_led.py
+++ analog_led.py
@@ -1,16 +1,26 @@
 import time
 import spidev
+
 spi = spidev.SpiDev()
 spi.open(0, 0)
+spi.max_speed_hz = 1000000
 
-def ReadChannel(channel):
-    adc = spi.xfer2([6 | (channel & 4) >> 2, (channel & 3) << 6, 0])
-    data = ((adc[1] & 15) << 8) + adc[2]
-    return data
 
-while True:
-    for i in range(8):
-        reading = ReadChannel(0)
-        voltage = (reading * 3.3) / 4096
-        print(f"Reading={reading}\t Voltage={voltage}")
-        time.sleep(0.2)
+def read_adc_ch0():
+
+
+    cmd = [0b00000110, 0b00000000, 0b00000000]
+    adc = spi.xfer2(cmd)
+    result = ((adc[1] & 0b00001111) << 8) | adc[2]
+    return result
+
+try:
+    while True:
+        value = read_adc_ch0()
+        voltage = (value * 3.3) / 4096
+        print(f"CH0: {value:4d} | {voltage:.2f} V")
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    spi.close()
+    print("\nSPI closed")
