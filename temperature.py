47199940f86068b0823f7e66e0b61c655f768e49
FriedStan <66070030@kmitl.ac.th>
2025-08-20 11:01:03 +0700

    files

--- temperature.py
+++ temperature.py
@@ -21,10 +21,10 @@ try:
     while True:
         reading_mpc = ReadChannel(0)
         reading_thermistor = ReadChannel(7)
-        voltage_mpc = (reading_mpc * 3.3) / 4096
+        voltage_mpc = (reading_mpc * 3.3) / 4095
         temp_mpc = 100*(((reading_mpc*3.3)/4095)-1)+50
 
-        voltage_thermistor = (reading_thermistor * 3.3) / 4096
+        voltage_thermistor = (reading_thermistor * 3.3) / 4095
         r_thermistor = (10000 * voltage_thermistor) / (3.3 - voltage_thermistor)
         temp_thermistor = (1 / ((1/(25+273.15)) + (1/4050) * math.log(r_thermistor/10000))) - 273.15
         print(f"Temp_mpc={temp_mpc:.2f}\t Temp_therm={temp_thermistor:.2f} {track}")
