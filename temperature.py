69ca415666881b8bd3a20ee4eaefc195ec7eb4a5
FriedStan <66070030@kmitl.ac.th>
2025-08-20 10:39:45 +0700

    files

--- temperature.py
+++ temperature.py
@@ -8,11 +8,7 @@ spi.max_speed_hz = 500000
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-ledpin = 12
 
-GPIO.setup(ledpin, GPIO.OUT)
-pi_pwm = GPIO.PWM(ledpin, 1000)
-pi_pwm.start(0)
 
 def ReadChannel(channel):
     adc = spi.xfer2([6 | (channel & 4) >> 2, (channel & 3) << 6, 0])
@@ -21,6 +17,7 @@ def ReadChannel(channel):
 
 try:
     while True:
+        track = "/"
         reading_mpc = ReadChannel(0)
         reading_thermistor = ReadChannel(7)
         voltage_mpc = (reading_mpc * 3.3) / 4096
@@ -29,8 +26,11 @@ try:
         voltage_thermistor = (reading_thermistor * 3.3) / 4096
         r_thermistor = (10000 * voltage_thermistor) / (3.3 - voltage_thermistor)
         temp_thermistor = (1 / ((1/(25+273.15)) + (1/4050) * math.log(r_thermistor/10000))) - 273.15
-        print(f"Reading={reading_mpc}\t Voltage={voltage_mpc:.2f}\t Temp_mpc={temp_mpc:.2f}\t Temp_therm={temp_thermistor:.2f}")
-        
+        print(f"Temp_mpc={temp_mpc:.2f}\t Temp_therm={temp_thermistor:.2f} {track}")
+        if track == "/":
+            track="\\"
+        else:
+            track="/"
         reading_thermistor = ReadChannel(7)
         time.sleep(0.3)
 
