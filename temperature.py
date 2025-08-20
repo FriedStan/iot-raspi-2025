46390e3dd6e5b879b70a1b1aad26c1213aa00260
FriedStan <66070030@kmitl.ac.th>
2025-08-20 10:06:30 +0700

    files

---
+++ temperature.py
@@ -0,0 +1,33 @@
+import time
+import spidev
+import RPi.GPIO as GPIO
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 1000000
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+ledpin = 12
+
+GPIO.setup(ledpin, GPIO.OUT)
+pi_pwm = GPIO.PWM(ledpin, 1000)
+pi_pwm.start(0)
+
+def read_adc_ch0():
+    cmd = [0b00000110, 0b00000000, 0b00000000]
+    adc = spi.xfer2(cmd)
+    result = ((adc[1] & 0b00001111) << 8) | adc[2]
+    return result
+
+try:
+    while True:
+        reading = read_adc_ch0()
+        voltage = (reading * 3.3) / 4096
+        temp = 100*(((reading*3.3)/4095)-1)+50
+        print(f"Reading={reading}\t Voltage={voltage:.2f}\t")
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    spi.close()
+    print("\nSPI closed")
